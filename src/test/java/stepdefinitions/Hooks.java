e4dd657eda4a4959b0f51ec4c905b8426d4562c7
olga <olga.scelokova@testdevlab.com>
2018-07-31 15:06:55 +0300

    Changes in launchBrowser function

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -3,17 +3,21 @@ package stepdefinitions;
 import com.codeborne.selenide.Configuration;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.CHROME;
+import static com.codeborne.selenide.WebDriverRunner.FIREFOX;
 
 
 public class Hooks {
     @Before
     public void launchBrowser(){
-        System.setProperty("selenide.browser", CHROME);
+        System.setProperty("webdriver.chrome.driver","/home/olga/Downloads/chromedriver");
+        Configuration.browser="chrome";
     }
 
     @Before
